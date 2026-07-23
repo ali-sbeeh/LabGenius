08533daea349740d6800523e7d4d9143f61e9fc9
ali sbeeh <alisbeeh440@gmail.com>
2026-07-23 22:05:59 +0300

    Fix: Correct namespace in ShippingCompanySeeder

--- database/seeders/ShippingCompanySeeder.php
+++ database/seeders/ShippingCompanySeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\models\ShippingCompany;
+use App\Models\ShippingCompany;
 class ShippingCompanySeeder extends Seeder
 {
     /**
