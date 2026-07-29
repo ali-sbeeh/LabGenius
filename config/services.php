497ba262da6f4d10b790737a27a85cecd358ea02
ali sbeeh <alisbeeh440@gmail.com>
2026-07-29 04:26:54 +0300

    Fix: Integrate Brevo API for production

--- config/services.php
+++ config/services.php
@@ -14,6 +14,7 @@ return [
     |
     */
 
+
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
     ],
@@ -33,6 +34,10 @@ return [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
+    ],
+
+     'brevo' => [
+        'key' => env('BREVO_API_KEY'),
     ],
 
 ];
